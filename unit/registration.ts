d779157aa7101610f3d5bd9197e3da4c2e183a72
Alina Kryuchkova <zuzikevich90@mail.ru>
2023-08-24 16:30:54 -0300

    add describe class Regisrtation

--- unit/registration.ts
+++ unit/registration.ts
@@ -1,49 +1,60 @@
 export default class Registration {
-    nameField: any;
-    loginFieald: any;
+    nameField: string;
+    loginField: string;
     passwordField: any;
 
-    constructor(nameField: string, loginFieald: any, passwordField: any) {
+    constructor(nameField: string, loginField: string, passwordField: any) {
         this.nameField = nameField;
-        this.loginFieald = loginFieald;
+        this.loginField = loginField;
         this.passwordField = passwordField;
     }
 
 
-    inputNameField() {
-        let answerName = '';
-        try {
-            this.nameField = oninput;
-            answerName = 'Success: Name field updated';
-        } catch (error) {
-            answerName = 'Error: Incorrect data entered';
+    validateNameField() {
+        const userName = this.nameField.replace(/\s/g, '');
+        const namePattern = /^[А-ЯЁа-яё]+$/;
+
+        if (!userName.match(namePattern)) {
+            return 'Error: Некорректные данные в поле "Имя"';
         }
 
-        return answerName;
+        return 'Success: Поле "Имя" обновлено';
     }
 
-    inputLoginField() {
-        let answerLogin = '';
-        try {
-            this.loginFieald = oninput;
-            answerLogin = 'Success: Name field updated';
-        }
-        catch (error) {
-            answerLogin = 'Error: Incorrect data entered';
-        }
-        return answerLogin;
-    };
+    validateLoginField() {
+        const userLogin = this.loginField.replace(/\s/g, '');
 
-    inputPasswordField() {
-        let answerPassword = '';
-        try {
-            this.passwordField = oninput;
-            answerPassword = 'Success: Name field updated';
+        if (!userLogin.match(/^[a-zA-Z]+$/)) {
+            return 'Error: Логин может содержать только латинские буквы';
         }
-        catch (error) {
-            answerPassword = 'Error: Incorrect data entered';
+
+        if (userLogin.length > 15) {
+            return 'Error: Логин не должен превышать 15 символов';
         }
-        return answerPassword;
-    };
+
+        return 'Success: Поле "Логин" обновлено';
+    }
+
+    validatePasswordField() {
+        const userPassword = this.passwordField.replace(/\s/g, '');
+
+        if (!userPassword.match(/^[a-zA-Z]+$/)) {
+            return 'Error: Пароль может содержать только латинские буквы';
+        }
+
+        if (!userPassword.match(/^[!?*]+$/)) {
+            return 'Error: Пароль должен содержать хотя бы один из симовлов "!?*"';
+        }
+
+        if (userPassword.length < 6) {
+            return 'Error: Пароль должен содержать не менее 6 символов';
+        }
+
+        if (userPassword.length > 12) {
+            return 'Error: Пароль не должен содержать более 12 символов';
+        }
+
+        return 'Success: Поле "Пароль" обновлено';
+    }
 
 }
