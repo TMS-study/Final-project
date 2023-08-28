70b2999798b7973e8f4fe355a57e629cf7bd5d8e
Alina Kryuchkova <zuzikevich90@mail.ru>
2023-08-28 18:45:26 -0300

    add mobile

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -1,49 +1,44 @@
-import { PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     timeout: 60 * 1000,
     expect: { timeout: 20000 },
-
+    
     use: {
         baseURL: process.env.BASE_URL,
         trace: 'on-first-retry',
         screenshot: 'only-on-failure',
-        viewport: { width: 1800, height: 1024 },
-        actionTimeout: 40 * 1000,
-        navigationTimeout: 30 * 1000
+        viewport: {width:1800, height: 1024},
+        actionTimeout: 60*1000,
+        navigationTimeout: 60*1000,
+        headless: false
     },
 
     projects: [
         {
-            
-            name: `Chrome`,
+            name: 'chrome',
             use: {
-
-                browserName: `chromium`,
-                channel: `chrome`,
-                headless: false,
-                viewport: { width: 1500, height: 730 },
-                ignoreHTTPSErrors: true,
-                acceptDownloads: true,
-                screenshot: `only-on-failure`,
-                video: `retain-on-failure`,
-                trace: `retain-on-failure`,
-
-
+                ...devices['Desktop Chrome'],
+                browserName: 'chromium',
             },
+        },
+
+
+        {
+            name: 'mobile',
+            use:{ ...devices['Galaxy S8']}
         },
     ],
 
-    workers: 3,
+    workers: 1,
     fullyParallel: true,
     reporter: [
         ['html'],
-        [
-            "allure-playwright",
+        ["allure-playwright",
             {
-                detail: true,
-                outputFolder: "allure-report",
-                suiteTitle: true,
+              detail: true,
+              outputFolder: "allure-report",
+              suiteTitle: true,
             }
         ],
     ],
